e8b91ce3480c70074ebcd1556c4213bb510f1d96
Oraion13 <65845210+Oraion13@users.noreply.github.com>
2022-05-14 09:32:48 +0530

    sql queries for all the tables written

---
+++ models/Files.php
@@ -0,0 +1,135 @@
+<?php
+
+// Operations for 
+// all the file storing tables
+// is handeled here
+class Add_description
+{
+    private $conn;
+
+    public $table = '';
+
+    public $id_name = '';
+    public $col_name = '';
+    public $col_type = '';
+    public $col = '';
+
+    public $id = 0;
+    public $purchase_id = 0;
+    public $col_name_value = '';
+    public $col_type_value = '';
+    public $col_value = '';
+
+    // Connect to the DB
+    public function __construct($db)
+    {
+        $this->conn = $db;
+    }
+
+    // Read all data
+    public function read()
+    {
+        $query = 'SELECT * FROM ' . $this->table;
+
+        $stmt = $this->conn->prepare($query);
+
+        if ($stmt->execute()) {
+            // If data exists, return the data
+            if ($stmt) {
+                return $stmt;
+            }
+        }
+
+        return false;
+    }
+
+    // Read all data by purchase_id
+    public function read_row()
+    {
+        $query = 'SELECT * FROM ' . $this->table . ' WHERE purchase_id = :purchase_id';
+
+        $stmt = $this->conn->prepare($query);
+
+        // Clean the data
+        $this->purchase_id = htmlspecialchars(strip_tags($this->purchase_id));
+
+        $stmt->bindParam(':purchase_id' , $this->purchase_id);
+
+        if ($stmt->execute()) {
+            // If data exists, return the data
+            if ($stmt) {
+                return $stmt;
+            }
+        }
+
+        return false;
+    }
+
+    // Insert user data
+    public function post()
+    {
+        $query = 'INSERT INTO ' . $this->table . ' SET purchase_id = :purchase_id, ' . $this->col_name . ' = :' . $this->col_name . ', '
+        . $this->col_type . ' = :' . $this->col_type . ', '
+        . $this->col . ' = :' . $this->col;
+
+        $stmt = $this->conn->prepare($query);
+
+        // Clean the data
+        $this->purchase_id = htmlspecialchars(strip_tags($this->purchase_id));
+        
+        $stmt->bindParam(':purchase_id', $this->purchase_id);
+        $stmt->bindParam(':' . $this->col_name, $this->col_name_value);
+        $stmt->bindParam(':' . $this->col_type, $this->col_type_value);
+        $stmt->bindParam(':' . $this->col, $this->col_value);
+
+        // If data inserted successfully, return True
+        if ($stmt->execute()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    // Update a field
+    public function update_row($to_update)
+    {
+        $to_set = $to_update . ' = :' . $to_update;
+        $query = 'UPDATE ' . $this->table . ' SET ' . $to_set . ' WHERE ' . $this->id_name . ' =: ' . $this->id_name;
+
+        $stmt = $this->conn->prepare($query);
+
+        $this->id = htmlspecialchars(strip_tags($this->id));
+        $this->col_value = htmlspecialchars(strip_tags($this->col_value));
+
+        $stmt->bindParam(':' . $to_update, $this->col_value);
+        $stmt->bindParam(':add_description_id', $this->id);
+
+        // If data updated successfully, return True
+        if ($stmt->execute()) {
+            return true;
+        }
+        return false;
+    }
+
+    // Delete data by id
+    public function delete_row()
+    {
+        $query = 'SELECT * FROM ' . $this->table . ' WHERE ' . $this->id_name . ' =: ' . $this->id_name;
+
+        $stmt = $this->conn->prepare($query);
+
+        // Clean the data
+        $this->id = htmlspecialchars(strip_tags($this->id));
+
+        $stmt->bindParam(':' . $this->id_name , $this->id);
+
+        if ($stmt->execute()) {
+            // If data exists, return the data
+            if ($stmt) {
+                return $stmt;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ models/Purchases.php
@@ -0,0 +1,142 @@
+<?php
+
+// Operations for 'purchase_purchases' is handeled here
+class Purchases
+{
+    private $conn;
+    private $table = 'purchase_purchases';
+
+    public $purchase_id  = 0;
+    public $purchase_type = '';
+    public $purchase_year = '';
+    public $department = '';
+    public $purchase_name = '';
+    public $purchase_purpose = '';
+    public $is_consumable = 0;
+    public $is_below = 0;
+
+    // Connect to the DB
+    public function __construct($db)
+    {
+        $this->conn = $db;
+    }
+
+
+    // Read all data
+    public function read()
+    {
+        $query = 'SELECT * FROM ' . $this->table ;
+
+        $stmt = $this->conn->prepare($query);
+
+        if ($stmt->execute()) {
+            // If data exists, return the data
+            if ($stmt) {
+                return $stmt;
+            }
+        }
+
+        return false;
+    }
+
+    // Read all data by ID
+    public function read_row()
+    {
+        $query = 'SELECT * FROM ' . $this->table . ' WHERE purchase_id = :purchase_id';
+
+        $stmt = $this->conn->prepare($query);
+
+        // Clean the data
+        $this->purchase_id = htmlspecialchars(strip_tags($this->purchase_id));
+
+        $stmt->bindParam(':purchase_id', $this->purchase_id);
+
+        if ($stmt->execute()) {
+            // Fetch the data
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            // If data exists, return the data
+            if ($row) {
+                return $row;
+            }
+        }
+
+        return false;
+    }
+
+    // Insert new purchase data
+    public function post()
+    {
+        $query = 'INSERT INTO ' . $this->table . ' SET purchase_type = :purchase_type, purchase_year = :purchase_year, 
+        department = :department, purchase_name = :purchase_name, purchase_purpose = :purchase_purpose, 
+        is_consumable = :is_consumable, is_below = :is_below';
+
+        $stmt = $this->conn->prepare($query);
+
+        // Clean the data
+        $this->purchase_type = htmlspecialchars(strip_tags($this->purchase_type));
+        $this->purchase_year = htmlspecialchars(strip_tags($this->purchase_year));
+        $this->department = htmlspecialchars(strip_tags($this->department));
+        $this->purchase_name = htmlspecialchars(strip_tags($this->purchase_name));
+        $this->purchase_purpose	= htmlspecialchars(strip_tags($this->purchase_purpose));
+        $this->is_consumable = htmlspecialchars(strip_tags($this->is_consumable));
+        $this->is_below = htmlspecialchars(strip_tags($this->is_below));
+
+        $stmt->bindParam(':purchase_type', $this->purchase_type);
+        $stmt->bindParam(':purchase_year', $this->purchase_year);
+        $stmt->bindParam(':department', $this->department);
+        $stmt->bindParam(':purchase_name', $this->purchase_name);
+        $stmt->bindParam(':purchase_purpose	', $this->purchase_purpose);
+        $stmt->bindParam(':is_consumable', $this->is_consumable);
+        $stmt->bindParam(':is_below', $this->is_below);
+
+        // If data inserted successfully, return True
+        if ($stmt->execute()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    // Update a field
+    public function update_row($to_update)
+    {
+        $to_set = $to_update . ' = :' . $to_update;
+        $query = 'UPDATE ' . $this->table . ' SET ' . $to_set . ' WHERE purchase_id  = :purchase_id ';
+
+        $stmt = $this->conn->prepare($query);
+
+        $this->$to_update = htmlspecialchars(strip_tags($this->$to_update));
+        $this->purchase_id  = htmlspecialchars(strip_tags($this->purchase_id ));
+
+        $stmt->bindParam(':' . $to_update, $this->$to_update);
+        $stmt->bindParam(':purchase_id ', $this->purchase_id );
+
+        // If data updated successfully, return True
+        if ($stmt->execute()) {
+            return true;
+        }
+        return false;
+    }
+
+    // Delete a purchase
+    public function delete_row()
+    {
+        $query = 'DELETE FROM ' . $this->table . ' WHERE purchase_id = :purchase_id';
+
+        $stmt = $this->conn->prepare($query);
+
+        // Clean the data
+        $this->purchase_id = htmlspecialchars(strip_tags($this->purchase_id));
+
+        $stmt->bindParam(':purchase_id', $this->purchase_id);
+
+        if ($stmt->execute()) {
+            if ($stmt) {
+                return $stmt;
+            }
+        }
+
+        return false;
+    }
+}
